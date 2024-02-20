0bb43ed5f9af0ed32285a112ddbd9d8423c5d845
james-burton-0 <ky23212@bristol.ac.uk>
2024-02-20 14:37:03 +0000

    asdka

---
+++ plotting.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+
+#Here we define our data
+times = [0, 1, 2, 3, 4, 5]
+speeds = [0, 1.5, 2, 2.5, 3, 3.5]
+#Create the plot
+plt.plot(times, speeds)
+#Display the plot
+plt.show()
